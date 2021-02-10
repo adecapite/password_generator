3bfb2126a76084eae00bf1a4faa992d2beeae88e
Anthony DeCapite <anthony.decapite@icloud.com>
2021-02-10 13:14:50 -0500

    Added prompt windows

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,39 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  if(generateBtn = true){
+    var length = window.prompt("How many characters would you like? (8-128 Character)")
+  }
+
+  if(length < 8 || length > 128)
+  {
+    length = window.prompt("Password must be between 8-128 characters.")
+  }
+// Asks user if they want special characters.
+  var SpecialCharacters = window.prompt("Do you want special characters? Y/N")
+
+  if(SpecialCharacters.toUpperCase === "Y" || SpecialCharacters.toUpperCase === "N")
+  {
+      if(SpecialCharacters.toUpperCase === "Y")
+      {
+        SpecialCharacters = true;
+  }else
+  {
+    SpecialCharacters = false;
+
+  }
+}
+
+{
+  SpecialCharacters = window.prompt ("Please use only captial Y and N for your response.")
+}
+ 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
