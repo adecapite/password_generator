706160dfb286bda81b62f331d6bd140384088196
Anthony DeCapite <anthony.decapite@icloud.com>
2021-02-10 20:09:57 -0500

    rewrote from if/else statements to switches for user usability and clean up.

--- Develop/script.js
+++ Develop/script.js
@@ -1,61 +1,83 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// Write password to the #password input
-function writePassword(){
+
+//Write password to thge #password input
+function generatePassword() {
+//Starts the generator
   if(generateBtn = true)
   {
-    var length = window.prompt("How many characters would you like? (8-128 Character)")
-    if(length < 8 || length > 128)
+    var pwlength = window.prompt("How many characters would you like? (8-128 Character)")
+    
+    //Catches if the password is shorter than 8 or longer than 128
+    if(pwlength < 8 || pwlength > 128)
     {
-      length = window.prompt("Password must be between 8-128 characters.")
+      pwlength = window.prompt("Password must be between 8-128 characters.")
     }
-   // Asks user if they want special characters.
-    var specialCharacters = window.prompt("Do you want special characters? Y/N")
-    if(specialCharacters.toUpperCase === "Y" || specialCharacters.toUpperCase === "N")
-    {
-      if(specialCharacters.toUpperCase === "Y")
-      {
-        specialCharacters = true;
-      }
-      else
-      {
-        specialCharacters = false;
-      }
-    }else;
-    
+    //Prompts if user needs special characters
+    var specialCharacters = window.confirm("Do you want special characters?")
+    //Asks if the user beeds uppercase letters
+    var upperCase = window.confirm("Do you want upper case letters?")
+    // Asks if user needs lower case letters
+    var lowerCase = window.confirm("Do you want lower case letters?")
+    // Asks if user needs numbers
+    var number = window.confirm("Do you want numbers?")
 
-    var upperCase = window.prompt ("Do you need an uppercase letter? Y/N" )
-      if(upperCase.toUpperCase === "Y" || specialCharacters.toUpperCase === "N")
+    if(!(specialCharacters) && !(upperCase) && !(lowerCase) && !(number))
     {
-      if(upperCase.toUpperCase === "Y")
+      window.alert("You must choose atleast one option.")
+    }else
+    {
+      var final = [];
+      switch(specialCharacters)
       {
-        upperCase = true;
+        case true:
+          var characters = ["!","@","#","$","%","*"];
+          final = final.concat(characters);
+          break;
+        case false:
+          break;
       }
-      else
+      switch (upperCase)
       {
-        upperCase = false;
+        case true:
+          var lettersUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+          final = final.concat(lettersUppercase)
+          break;
+        case false:
+          break;
+        
       }
-    }else;
-    
+      switch(lowerCase)
+      {
+        case true:
+          var lettersLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+          final = final.concat (lettersLowercase);
+          break;
+        case false:
+          break;
+      }
+     
+    }
 
-    var lowerCase = window.prompt ("Do you need lower case letters? Y/N" )
-    if(upperCase.toUpperCase === "Y" || specialCharacters.toUpperCase === "N")
-    {
-      if(upperCase.toUpperCase === "Y")
-      {
-        upperCase = true;
-      }
-      else
-      {
-        upperCase = false;
-      }
-    }else;
-    
-  }
+    password = final.join("");
+
+
 
   
+  
+  
+  }
 
-
+  charset = password;
+  retVal = "";
+  for (var i = 0, n = charset.length; i < pwlength; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+// Write password to the #password input
+function writePassword()
+{
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
